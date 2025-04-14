937362f4ffa4e3c296482f8776b989be7338bf28
felipedabed <57273007+felipedabed@users.noreply.github.com>
2025-04-14 16:23:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 # Cargar datos
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/Resultados_ROL.csv", delimiter=",", encoding="ISO-8859-1")
+    df = pd.read_csv("data/Resultados_ROL.csv", delimiter=",", encoding="utf-8")
     
     # Limpiar nombres de columnas
     df.columns = df.columns.str.strip().str.replace("\u00a0", " ").str.replace("\ufeff", "")
